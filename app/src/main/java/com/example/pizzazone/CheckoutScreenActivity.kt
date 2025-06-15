22fcb88dae313c2ff14f84670cedb1099fa42224
Kulunu1317 <kulunuthathsara13@gmail.com>
2025-06-16 00:05:34 +0530

    Set fragment Screens

---
+++ app/src/main/java/com/example/pizzazone/CheckoutScreenActivity.kt
@@ -0,0 +1,7 @@
+package com.example.pizzazone
+
+import androidx.appcompat.app.AppCompatActivity
+
+class CheckoutScreenActivity:AppCompatActivity() {
+
+}
--- app/src/main/java/com/example/pizzazone/HomeFragment.kt
+++ app/src/main/java/com/example/pizzazone/HomeFragment.kt
@@ -1,20 +1,52 @@
 package com.example.pizzazone
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class HomeFragment:Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // Inflate the layout and assign to variable
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Access the button inside the view
+        val buttonlist = view.findViewById<Button>(R.id.buttonlist)
+
+        // Set click listener
+        buttonlist.setOnClickListener {
+            val intent = Intent(activity, ListScreenActivity::class.java)
+            intent.putExtra("showListFragment", true)
+            startActivity(intent)
+        }
+
+        // Return the view
+        return view
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
