cb39fff78cc09214e686007b75a4f7f7570e9ea2
Kulunu1317 <kulunuthathsara13@gmail.com>
2025-06-18 21:06:15 +0530

    Fix button Navigation

--- app/src/main/java/com/example/pizzazone/MyOrderFragment.kt
+++ app/src/main/java/com/example/pizzazone/MyOrderFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
-class MyOrderFragment :Fragment() {
+class MyOrderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myorder, container, false)
+        val view = inflater.inflate(R.layout.fragment_myorder, container, false)
+
+        val backArrow = view.findViewById<ImageView>(R.id.shipping_back_arrow)
+
+        backArrow.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        return view
     }
 }
