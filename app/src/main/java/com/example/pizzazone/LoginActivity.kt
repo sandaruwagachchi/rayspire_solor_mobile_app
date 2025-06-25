83cf237cd1b817f436f7ab01c35f93c40e4128fd
Kulunu1317 <kulunuthathsara13@gmail.com>
2025-06-25 20:15:40 +0530

    Add more UI

--- app/src/main/java/com/example/pizzazone/LoginActivity.kt
+++ app/src/main/java/com/example/pizzazone/LoginActivity.kt
@@ -14,9 +14,15 @@ class LoginActivity:AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val buttonlog = findViewById<Button>(R.id.buttonLogin)
+<<<<<<< Updated upstream
         val textViewreg = findViewById<TextView>(R.id.textViewreg)
         val buttonadmin = findViewById<Button>(R.id.buttonadmin)
         val textViewForgot =findViewById<TextView>(R.id.textViewForgot)
+=======
+        val textViewreg = findViewById<TextView>(R.id.textViewSignUp)
+        val buttonadmin = findViewById<Button>(R.id.buttonAdminLogin)
+        val textViewForgot =findViewById<TextView>(R.id.textViewForgotPassword)
+>>>>>>> Stashed changes
 
 
         buttonlog.setOnClickListener{
---
+++ app/src/main/java/com/example/pizzazone/Order.kt
@@ -0,0 +1,6 @@
+package com.example.pizzazone.model
+
+data class Order(    val id: String,
+                     val address: String,
+                     val price: String,
+                     val date: String)
