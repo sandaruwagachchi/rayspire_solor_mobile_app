86850f508fd9ab1f1dbb86466aeabccf2bb7840f
Thenula09 <thenulahansaja12@gmail.com>
2025-07-25 08:03:52 +0530

    erra complte

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.firebase.storage.ktx.v2100)
 
     implementation(libs.litert.support.api)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
